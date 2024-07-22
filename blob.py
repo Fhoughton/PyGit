695c8df8cbd433ecc987146c2f7c2c37c48c2e82
FHoughton <fdphoughton@protonmail.com>
2024-07-22 10:44:41 +0100

    Add code for basic git object / blob support

---
+++ blob.py
@@ -0,0 +1,12 @@
+from git_object import GitObject
+
+# Blobs are a git object that holds raw data from the user (e.g. files like main.c)
+class Blob(GitObject):
+    def get_name(self):
+        return b"blob"
+
+    def serialize(self):
+        return self.data
+
+    def deserialize(self, data):
+        self.data = data
--- repo.py
+++ repo.py
@@ -1,5 +1,6 @@
 import os
-
+import zlib
+import hashlib
 
 class GitRepo:
     """Represents a git repository (working tree and git directory)"""
@@ -51,3 +52,48 @@ class GitRepo:
             os.mkdir(resolved_path)
 
         return resolved_path
+
+    def read_object(self, hash):
+        """Reads a git object from its hash and returns it"""
+        object_path = self.gitdir_path("objects", sha[0:2], sha[2:]) # sha [0:2] as first 2 characters of hash are folder name, then rest file name
+
+        with open(object_path, "rb") as object_bin:
+            object_data = zlib.decompress(object_bin.read()) # By default git objects are compressed
+
+            # Git object headers begin with the name e.g. blob and then a space, so we find the space first
+            space_index = object_data.find(b' ')
+            object_type = object_data[0:space_index]
+
+            # After the space is the object size followed by a null byte (0x00)
+            null_index = object_data.find(b'\x00', space_index) # Start searching after the space, technically not needed
+
+            object_size = int(object_data[space_index:null_index].decode("ascii"))
+
+            # Construct a class based upon what object type it is
+            match object_type:
+                case b'commit': pass
+                case b'blob': pass
+                case b'tree': pass
+                case b'tag': pass
+                case _: raise Exception(f"Unrecognized object type for {hash}, found '{object_type}' not commit, blob, tree or tag")
+
+    def write_object(self, obj):
+        """Writes a GitObject's data to the correct path and file, formatted correctly"""
+        # Get the object as useable bytes
+        object_data = obj.serialize()
+
+        # Construct the object header
+        object_data = obj.get_name() + b' ' + str(len(object_data)).encode() + b'\x00' + object_data
+
+        # Hash the object
+        object_hash = hashlib.sha1(object_data).hexdigest()
+
+        # Resolve the path for the object to be stored in
+        object_path = self.gitdir_path("objects", object_hash[0:2], object_hash[2:], mkdir=True)
+
+        # Write the object data to the file
+        with open(object_path, "wb") as object_file:
+            object_file.write(zlib.compress(object_data))
+
+        # Return the hash to expose a reference for the new object
+        return object_hash
